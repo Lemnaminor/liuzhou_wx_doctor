bd52156cd215581f9d281a11926650086f33f5d7
Lemnaminor <291294950@qq.com>
2019-07-07 20:58:35 +0800

    2019-07-07 20:58 梁恒

---
+++ pages/editFirst/editFirst.js
@@ -0,0 +1,285 @@
+// pages/editFirst/editFirst.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+    // 路由传参
+    doctorId: '', // 医生ID
+
+    //设置用户信息
+    userList: [{
+      userName: "未获取",
+      userHeaderUrl: ""
+    }],
+
+    // 职称列表数据
+    selectWorkList: [],
+    selectWorkListIndex: null,
+
+    // 科室列表数据
+    selectDepartmentList: [],
+    selectDepartmentListIndex: null,
+
+
+    // 医生简介-描述信息
+    placeholder: "请填写医生简介描述信息...",
+    textMaxLength: 1000,
+    writeLength: 0,
+    isShowMask: true,
+    content: '',
+
+    // 医生擅长-描述信息
+    placeholder2: "请填写医生擅长描述信息...",
+    textMaxLength2: 1000,
+    writeLength2: 0,
+    isShowMask2: true,
+    content2: '',
+
+  },
+
+  /**
+   * 自定义函数事件
+   */
+
+
+  // 获取职称列表数据
+  getWorkList() {
+    var that = this;
+    wx.request({
+      url: getApp().globalData.path + `/enterprise/titlelist`,
+      data: {},
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      method: 'GET',
+      success: function (res) {
+        console.log('***** 获取职称列表数据 *****');
+        console.log(res);
+        that.setData({
+          selectWorkList: res.data.data
+        })
+      },
+      fail: function () {
+
+      }
+    })
+  },
+
+  // 选择职称
+  selectWorkList: function (e) {
+    console.log(e);
+    console.log('选择职称改变，携带值为', e.detail.value);
+    this.setData({
+      selectWorkListIndex: e.detail.value
+    })
+  },
+
+  // 获取科室列表数据
+  getDepartmentList() {
+    var that = this;
+    wx.request({
+      url: getApp().globalData.path + `/enterprise/departmentlist`,
+      data: {},
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      method: 'GET',
+      success: function(res) {
+        console.log('***** 获取科室列表数据 *****');
+        console.log(res);
+        that.setData({
+          selectDepartmentList: res.data.data
+        })
+      },
+      fail: function() {
+
+      }
+    })
+  },
+
+  // 选择科室
+  selectDepartmentList: function(e) {
+    console.log('选择科室改变，携带值为', e.detail.value);
+    this.setData({
+      selectDepartmentListIndex: e.detail.value
+    })
+  },
+
+  // 提交保存
+  editDataFormSubmit(e) {
+    console.log(`***** 提交保存 *****`);
+    console.log(e);
+    var that = this;
+    var editDataFormList = e.detail.value;
+    var doctorId = that.data.doctorId;
+
+    wx.request({
+      url: getApp().globalData.path + `/enterprise/submitEdictDoctor`,
+      data: {
+        'id': e.detail.value.id,
+        'telPhone': e.detail.value.telPhone,
+        'doctorLevel': e.detail.value.doctorLevel,
+        'doctorLevelDict': e.detail.value.doctorLevelDict,
+        'dept_Id': e.detail.value.dept_Id,
+        // 'deptName': e.detail.value.deptName,
+        'workerNumber': e.detail.value.workerNumber,
+        'doctorIntroduction': e.detail.value.doctorIntroduction,
+        'doctorSkill': e.detail.value.doctorSkill
+      },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      method: 'POST',
+      success: function (res) {
+        console.log('***** 编辑资料表单提交成功 *****');
+        console.log(res);
+        if (res.data.code == 0) {
+          wx.showToast({
+            title: '保存成功',
+            icon: 'success'
+          })
+          wx.navigateTo({
+            url: '/pages/editDataSuccess/editDataSuccess',
+          })
+        } else {
+          wx.showToast({
+            title: '保存失败',
+            icon: 'none'
+          })
+        }
+      },
+      fail: function () {
+
+      }
+    })
+
+    // wx.navigateTo({
+    //   url: '/pages/editDataSuccess/editDataSuccess',
+    // })
+  },
+
+  // 医生简介-字数输入
+  bindText(e) {
+    console.log('***** 医生简介-字数输入 *****')
+    var that = this;
+    var len = e.detail.value.length;
+    var cont = e.detail.value;
+    that.setData({
+      writeLength: len,
+      content: cont
+    })
+  },
+
+  // 医生擅长-字数输入
+  bindText2(e) {
+    console.log('***** 医生擅长-字数输入 *****')
+    var that = this;
+    var len = e.detail.value.length;
+    var cont = e.detail.value;
+    that.setData({
+      writeLength2: len,
+      content2: cont
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    console.log(`***** 进入编辑资料页面 *****`);
+    this.setData({
+      doctorId: getApp().globalData.doctorId
+    })
+    this.getDepartmentList(); // 调用科室列表接口
+    this.getWorkList(); // 调用职称列表接口
+
+    let that = this;
+    //登录的信息创建
+    wx.login({
+      success: function (e) {
+        wx.setStorage({
+          key: "key",
+          data: e.errMsg
+        })
+      }
+    })
+    //获取用户的信息
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(`获取用户的信息`);
+        console.log(res);
+        let userInfo = res.userInfo
+        let nickName = userInfo.nickName
+        let avatarUrl = userInfo.avatarUrl
+        //先将信息存放到本地
+        wx.setStorageSync('nickName', nickName);
+        wx.setStorageSync('avatarUrl', avatarUrl);
+
+        let user = [{
+          userName: nickName,
+          userHeaderUrl: avatarUrl,
+        }]
+        that.setData({
+          userList: user,
+        })
+      },
+      fail: function (res) {
+        console.log(res);
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/inspect/inspect.js
+++ pages/inspect/inspect.js
@@ -6,12 +6,142 @@ Page({
    */
   data: {
 
+    // 患者ID
+    PatientId: '0000676543',
+
+    // 检验报告列表数据
+    inspectList: [{
+      "createDate": "2019-06-27 08:36:55",
+      "reportNo": "6758081",     //编号
+      "itemClass": "甲功5",   //项目类
+      "itemSubClass": "血清",  //项目子类
+      "reportDate": "2019-06-27 09:42:04",  //报告时间
+      "reportStatus": "report"
+    },
+      {
+        "createDate": "2019-06-26 17:42:27",
+        "reportNo": "6756239",
+        "itemClass": "(J)肾4,(J)糖,(J)心4,(J)电4,(J)肝10",
+        "itemSubClass": "血清",
+        "reportDate": "2019-06-26 18:11:20",
+        "reportStatus": "report"
+      }, {
+        "createDate": "2019-06-27 08:36:55",
+        "reportNo": "6758081",     //编号
+        "itemClass": "甲功5",   //项目类
+        "itemSubClass": "血清",  //项目子类
+        "reportDate": "2019-06-27 09:42:04",  //报告时间
+        "reportStatus": "report"
+      },
+      {
+        "createDate": "2019-06-26 17:42:27",
+        "reportNo": "6756239",
+        "itemClass": "(J)肾4,(J)糖,(J)心4,(J)电4,(J)肝10",
+        "itemSubClass": "血清",
+        "reportDate": "2019-06-26 18:11:20",
+        "reportStatus": "report"
+      }, {
+        "createDate": "2019-06-27 08:36:55",
+        "reportNo": "6758081",     //编号
+        "itemClass": "甲功5",   //项目类
+        "itemSubClass": "血清",  //项目子类
+        "reportDate": "2019-06-27 09:42:04",  //报告时间
+        "reportStatus": "report"
+      },
+      {
+        "createDate": "2019-06-26 17:42:27",
+        "reportNo": "6756239",
+        "itemClass": "(J)肾4,(J)糖,(J)心4,(J)电4,(J)肝10",
+        "itemSubClass": "血清",
+        "reportDate": "2019-06-26 18:11:20",
+        "reportStatus": "report"
+      }],
+    pageIndex: 1, // 请求页索引
+    pageNum: 5, // 请求数据条数
+    pageCount: 0, // 总页数
+    amount: 0, // 总条数
+
+  },
+
+  /**
+   * 自定义函数事件
+   */
+  // 检验报告列表接口
+  inspectList(){
+    var that = this;
+    console.log(`请求检验报告接口：当前页：${that.data.pageIndex},总页数：${that.data.pageCount},显示条数：${that.data.pageNum}`);
+    wx.request({
+      url: getApp().globalData.path + `/enterprise/inspectTestList`,
+      data: {
+        PatientId: that.data.PatientId
+      },
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log("***** 检验报告接口成功 *****")
+        console.log(res);
+        if (res.data.code == 0) {
+          wx.showLoading({
+            title: '数据加载中',
+          })
+          var tempList = that.data.inspectList
+          var tempPageIndex = that.data.pageIndex;
+          if (that.data.pageIndex == 1) {
+            tempList = res.data.data.list;
+            tempPageIndex = 1;
+          } else {
+            tempList = tempList.concat(res.data.data.list);
+          }
+          that.setData({
+            pageIndex: tempPageIndex,
+            pageNum: res.data.data.pageSize,
+            pageCount: res.data.data.pages,
+            amount: res.data.data.total,
+            inspectList: tempList
+          });
+          wx.hideLoading();
+
+        } else {
+          wx.showToast({
+            title: '网络请求错误',
+            icon: 'none'
+          })
+        }
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+
+  // 跳转检验报告详情
+  toInspectDetail(){
+    console.log(`***** 跳转检验报告详情 *****`);
+    var that = this;
+    wx.navigateTo({
+      url: `/pages/inspectDetail/inspectDetail?PatientId=${that.data.PatientId}`,
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+    console.log(`***** 进入检验报告页面 *****`);
+
+    wx.showLoading({
+      title: '数据加载中',
+    })
+
+    // this.inspectList();
+
+    wx.hideLoading();
 
   },
 
@@ -48,12 +178,40 @@ Page({
    */
   onPullDownRefresh: function () {
 
+    console.log('下拉刷新');
+    wx.showLoading({
+      title: '数据加载中'
+    })
+    this.data.pageIndex = 1;
+    this.inspectList(); // 游客评价接口
+    wx.hideLoading();
+    wx.showToast({
+      title: '数据已刷新',
+      icon: 'success',
+      duration: 1500
+    })
+
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+
+    console.log("上拉触底");
+
+    console.log(`页数：${this.data.pageIndex}，总页数：${this.data.pageCount}`);
+    if (this.data.pageIndex < this.data.pageCount) {
+      this.data.pageIndex += 1;
+      console.log(`当前页数：${this.data.pageIndex}`);
+      this.inspectList();
+    } else {
+      wx.showToast({
+        title: '没有更多数据了',
+        icon: 'none'
+      })
+    }
 
   },
 
